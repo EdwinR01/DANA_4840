4044b8805b767771b4018db458f081bc34610dbf
Guneet <guneetsharma18@gmail.com>
2024-03-06 17:25:38 -0800

    Stats analysis

---
+++ Final Cancer.R
@@ -0,0 +1,5 @@
+```{r}
+data_cancer <- read.csv('/Users/guneetsharma/Desktop/SEMESTER\ 3/DANA\ 4840/DANA\ 4840\ final\ project\ Cancer/archive-4/data.csv')
+head(data_cancer)
+
+install.packages("tidyverse")
